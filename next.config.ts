59d7069cc095207b1344bbac36623d9933939985
Robert Guss <robertguss@gmail.com>
2025-06-09 22:10:53 -0400

    update next config to allow images from convex url

--- next.config.ts
+++ next.config.ts
@@ -5,11 +5,21 @@ const nextConfig: NextConfig = {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: '**.convex.cloud',
+        hostname: '*.convex.cloud',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'judicious-horse-579.convex.cloud',
+        port: '',
+        pathname: '/**',
       },
       {
         protocol: 'http',
         hostname: 'localhost',
+        port: '',
+        pathname: '/**',
       },
     ],
   },
